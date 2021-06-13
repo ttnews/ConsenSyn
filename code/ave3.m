be39de8421b2320ab7973c99a49e57fde45b28ad
ttnews <64695946+ttnews@users.noreply.github.com>
2021-06-13 20:16:46 +0100

    Update ave3.m

--- code/ave3.m
+++ code/ave3.m
@@ -1,9 +1,4 @@
 function y=ave3(t,x2,x5,x6)
-%x2 is trusted
-delta=1;
-if (abs(rang2(x2)-rang5(x5))>delta)||(abs(rang2(x2)-rang6(x6))>delta)||(abs(rang5(x5)-rang6(x6))>delta)
-y=rang2(x2);else
-y=(rang2(x2)+rang5(x5)+rang6(x6))/3;end
 
 
 
